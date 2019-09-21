b218e3479f4327ec8f56a99e5236c1129fb31430
Adriano Ruseler <ruseler@inep.ufsc.br>
2019-09-21 18:07:45 -0300

    Update Moodle37update.sh

--- Moodle37update.sh
+++ Moodle37update.sh
@@ -12,21 +12,17 @@ fi
 echo "Download OK..."
 
 echo "Download Plugins..."
-wget https://github.com/AdrianoRuseler/moodle-plugins/archive/MOODLE_37_STABLE.zip -O MOODLE_37_PLUGINS_STABLE.zip
-if [[ $? -ne 0 ]] ; then
-    exit 1
-fi
-echo "Download OK..."
-
-echo "Extract and move Plugins..."
-unzip MOODLE_37_PLUGINS_STABLE.zip
-mv moodle-plugins-MOODLE_37_STABLE/moodle moodle
+git clone https://github.com/AdrianoRuseler/moodle-plugins.git
+cd moodle-plugins
+git submodule update --init --recursive
+cd ..
+mv moodle-plugins/moodle moodle
 
 echo "Extract Moodle 37..."
 tar xvzf moodle-latest-37.tgz
 
 echo "Clean files..."
-rm -rf moodle-latest-37.tgz MOODLE_37_PLUGINS_STABLE.zip moodle-plugins-MOODLE_37_STABLE
+rm -rf moodle-latest-37.tgz moodle-plugins
 
 # echo "Activating Moodle Maintenance Mode in...";
 sudo -u www-data /usr/bin/php $MOODLE_HOME/admin/cli/maintenance.php --enablelater=1
