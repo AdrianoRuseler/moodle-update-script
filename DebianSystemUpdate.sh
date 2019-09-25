86044fb154a50a7ebef4d9b2efdb4d2794eacb7c
Adriano Ruseler <ruseler@inep.ufsc.br>
2019-09-25 10:54:09 -0300

    Update DebianSystemUpdate.sh

--- DebianSystemUpdate.sh
+++ DebianSystemUpdate.sh
@@ -7,10 +7,16 @@ echo "Autoremove and Autoclean System..."
 sudo apt-get autoremove -y && sudo apt-get autoclean -y
 
 echo "Install some sys utils..."
-sudo apt-get install -y git gcc
+sudo apt-get install -y git
 
 echo "Install python..."
 sudo apt-get install -y python2 python3
+
+echo "Install maxima (Stack question type for Moodle) ..."
+sudo apt-get install -y maxima
+
+echo "To be able to generate graphics from DOT files, you must have installed the dot executable..."
+sudo apt-get install -y graphviz
 
 echo "To use spell-checking within the editor, you MUST have aspell 0.50 or later installed on your server..."
 sudo apt-get install -y aspell dictionaries-common libaspell15 aspell-de aspell-es aspell-fr aspell-en aspell-pt-br aspell-pt-pt aspell-doc spellutils
