9d3ad7deb4757d7a268ec9b9b560275c66d3bf9e
Adriano Ruseler <ruseler@inep.ufsc.br>
2019-09-25 10:35:12 -0300

    Update DebianSystemUpdate.sh

--- DebianSystemUpdate.sh
+++ DebianSystemUpdate.sh
@@ -9,6 +9,9 @@ sudo apt-get autoremove -y && sudo apt-get autoclean -y
 echo "Install some sys utils..."
 sudo apt-get install -y git gcc
 
+echo "Install python..."
+sudo apt-get install -y python2 python3
+
 echo "To use spell-checking within the editor, you MUST have aspell 0.50 or later installed on your server..."
 sudo apt-get install -y aspell dictionaries-common libaspell15 aspell-de aspell-es aspell-fr aspell-en aspell-pt-br aspell-pt-pt aspell-doc spellutils
 
