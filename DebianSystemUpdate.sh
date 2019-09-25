c5e4329fafc8cfc72862d9776997ed15def719d9
Adriano Ruseler <ruseler@inep.ufsc.br>
2019-09-25 10:08:19 -0300

    Update DebianSystemUpdate.sh

--- DebianSystemUpdate.sh
+++ DebianSystemUpdate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y
+sudo apt-get update && sudo apt-get upgrade -y
 
-sudo apt autoremove -y
+sudo apt-get autoremove -y && sudo apt-get autoclean -y
