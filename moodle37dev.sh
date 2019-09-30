9c2f10239414258fd82aa6f1620e895377f7e90d
Adriano Ruseler <ruseler@inep.ufsc.br>
2019-09-30 18:10:55 -0300

    Update moodle37dev.sh

--- moodle37dev.sh
+++ moodle37dev.sh
@@ -93,6 +93,27 @@ else
 fi
 
 echo "update remote"
+echo "Check MD5 (128-bit) checksums, same version tested?"
+md5sum -c $GIT_DIR/moodle37-plugins/moodle-latest-37.tgz.md5
+if [[ $? -ne 0 ]] ; then
+    exit 1    
+fi
+echo "OK!"
+
+cd $GIT_DIR/moodle37-plugins/
+wget https://download.moodle.org/download.php/direct/stable37/moodle-latest-37.tgz.md5 -O moodle-latest-37.tgz.md5
+md5sum -c $GIT_DIR/moodle37-plugins/moodle-latest-37.tgz.md5
+if [[ $? -ne 0 ]] ; then
+    echo "Same moodle-latest-37 version!"
+    else
+    echo "Updated moodle-latest-37 version! Download new version..."
+    rm moodle-latest-37.tgz
+    wget https://download.moodle.org/download.php/direct/stable37/moodle-latest-37.tgz -O moodle-latest-37.tgz
+    if [[ $? -ne 0 ]] ; then
+      exit 1
+    fi    
+fi
+echo "OK!"
 
 
 echo "Rsync moodle folder from moodle37-plugins repo..."
