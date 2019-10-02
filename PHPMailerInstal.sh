147120dff27c65fa7e9bd96c2fc9ea5f350d9bf4
Adriano Ruseler <ruseler@inep.ufsc.br>
2019-10-02 13:05:26 -0300

    Update PHPMailerInstal.sh

--- PHPMailerInstal.sh
+++ PHPMailerInstal.sh
@@ -1,4 +1,4 @@
-sudo apt install wget php-cli php-zip unzip
+# sudo apt install wget php-cli php-zip unzip
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
