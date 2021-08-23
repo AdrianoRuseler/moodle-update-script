8508829de864dfbc1810572725826aa536e0fb52
Adriano Ruseler <ruseler@inep.ufsc.br>
2021-08-23 12:46:48 -0300

    Create SystemUpdate.sh

---
+++ SystemUpdate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "##--------------------- SYSTEM INFO --------------------------##"
+uname -a # Gets system info
+echo ""
+df -H # Gets disk usage info
+echo ""
+date # Gets date
+
+echo ""
+echo "##----------------------- SYSTEM UPGRADE ------------------------##"
+echo "Update and Upgrade System..."
+sudo apt-get update 
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -o Dpkg::Options::=--force-confold -o Dpkg::Options::=--force-confdef
+
+echo "Autoremove and Autoclean System..."
+sudo apt-get autoremove -y && sudo apt-get autoclean -y
+
+
+echo ""
+date # Gets date
