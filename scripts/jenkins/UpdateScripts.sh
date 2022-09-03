bdb315e7d4f5bce9f1282c45035cabf20a4ea6f7
Adriano Ruseler <ruseler@inep.ufsc.br>
2022-09-03 10:45:52 -0300

    Update UpdateScripts.sh
    Fixed url

--- scripts/jenkins/UpdateScripts.sh
+++ scripts/jenkins/UpdateScripts.sh
@@ -1,93 +1,93 @@
 #!/bin/bash
 
 rm CreateApacheLocalSite.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/CreateApacheLocalSite.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/CreateApacheLocalSite.sh
 chmod a+x CreateApacheLocalSite.sh
 
 rm CreateApacheSite.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/CreateApacheSite.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/CreateApacheSite.sh
 chmod a+x CreateApacheSite.sh
 
 rm CreateDataBaseUser.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/CreateDataBaseUser.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/CreateDataBaseUser.sh
 chmod a+x CreateDataBaseUser.sh
 
 rm DeleteApacheLocalSite.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/DeleteApacheLocalSite.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/DeleteApacheLocalSite.sh
 chmod a+x DeleteApacheLocalSite.sh
 
 rm DropMDL.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/DropMDL.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/DropMDL.sh
 chmod a+x DropMDL.sh
 
 rm GetMDL.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/GetMDL.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/GetMDL.sh
 chmod a+x GetMDL.sh
 
 rm InstallMDL.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/InstallMDL.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/InstallMDL.sh
 chmod a+x InstallMDL.sh
 
 rm UpdateMDL.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/UpdateMDL.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/UpdateMDL.sh
 chmod a+x UpdateMDL.sh
 
 rm ConfigMDL.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/ConfigMDL.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/ConfigMDL.sh
 chmod a+x ConfigMDL.sh
 
 rm InstallPHUnitMDL.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/InstallPHUnitMDL.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/InstallPHUnitMDL.sh
 chmod a+x InstallPHUnitMDL.sh
 
 rm FixMDL.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/FixMDL.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/FixMDL.sh
 chmod a+x FixMDL.sh
 
 rm DeleteMDLSite.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/DeleteMDLSite.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/DeleteMDLSite.sh
 chmod a+x DeleteMDLSite.sh
 
 rm BackUpMDL.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/BackUpMDL.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/BackUpMDL.sh
 chmod a+x BackUpMDL.sh
 
 rm ClearBackUpMDL.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/ClearBackUpMDL.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/ClearBackUpMDL.sh
 chmod a+x ClearBackUpMDL.sh
 
 rm RestoreMDL.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/RestoreMDL.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/RestoreMDL.sh
 chmod a+x RestoreMDL.sh
 
 rm CleanUpEnvFile.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/CleanUpEnvFile.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/CleanUpEnvFile.sh
 chmod a+x CleanUpEnvFile.sh
 
 rm CourseBackUpMDL.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/CourseBackUpMDL.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/CourseBackUpMDL.sh
 chmod a+x CourseBackUpMDL.sh
 
 rm CourseRestoreMDL.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/CourseRestoreMDL.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/CourseRestoreMDL.sh
 chmod a+x CourseRestoreMDL.sh
 
 rm GetGeoLite2.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/GetGeoLite2.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/GetGeoLite2.sh
 chmod a+x GetGeoLite2.sh
 
 rm PurgeMDL.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/PurgeMDL.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/PurgeMDL.sh
 chmod a+x PurgeMDL.sh
 
 rm InstallPMA.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/InstallPMA.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/InstallPMA.sh
 chmod a+x InstallPMA.sh
 
 rm UpdatePMA.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/UpdatePMA.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/UpdatePMA.sh
 chmod a+x UpdatePMA.sh
 
 rm SystemUpdate.sh
-wget https://raw.githubusercontent.com/AdrianoRuseler/moodledev-plugins/main/scripts/SystemUpdate.sh
+wget https://raw.githubusercontent.com/AdrianoRuseler/moodle-update-script/master/scripts/jenkins/SystemUpdate.sh
 chmod a+x SystemUpdate.sh
