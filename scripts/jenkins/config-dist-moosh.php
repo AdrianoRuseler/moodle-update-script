839a9193795fe7d4aaee63bbdd77dd4a6294e152
Adriano Ruseler <ruseler@inep.ufsc.br>
2022-09-04 10:30:05 -0300

    Create config-dist-moosh.php

---
+++ scripts/jenkins/config-dist-moosh.php
@@ -0,0 +1,46 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mydbtype';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'mydbname';
+$CFG->dbuser    = 'mydbuser';
+$CFG->dbpass    = 'mydbpass';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+
+$CFG->wwwroot   = 'mysiteurl';
+$CFG->dataroot  = 'mydatafolder';
+$CFG->admin     = 'admin';
+$CFG->directorypermissions = 0777;
+
+// Use the following flag to completely disable the installation of plugins
+// (new plugins, available updates and missing dependencies) and related
+// features (such as cancelling the plugin installation or upgrade) via the
+// server administration web interface.
+$CFG->disableupdateautodeploy = true;
+// Disabling update notifications
+$CFG->disableupdatenotifications = true;
+
+// Some administration options allow setting the path to executable files. This can
+// potentially cause a security risk. Set this option to true to disable editing
+// those config settings via the web. They will need to be set explicitly in the
+// config.php file
+$CFG->preventexecpath = true;
+
+// Force result of checks used to determine whether a site is considered "public" or not (such as for site registration).
+$CFG->site_is_public = false;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
