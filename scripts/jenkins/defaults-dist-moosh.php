1631bd9244a533f8aea9071649f41ba587669017
Adriano Ruseler <ruseler@inep.ufsc.br>
2022-09-04 10:32:28 -0300

    Create defaults-dist-moosh.php

---
+++ scripts/jenkins/defaults-dist-moosh.php
@@ -0,0 +1,22 @@
+<?php
+$defaults['moodle']['summary'] = 'Moodle Site! '; // for core settings
+$defaults['moodle']['custommenuitems'] = 'Theme
+-Boost | ?theme=boost
+-Classic | ?theme=classic
+';
+
+$defaults['moodle']['timezone'] = 'America/Sao_Paulo';
+$defaults['moodle']['defaultcity'] = 'Curitiba';
+
+$defaults['moodle']['pathtophp'] = '/usr/bin/php';
+$defaults['moodle']['pathtodu'] = '/usr/bin/du';
+$defaults['moodle']['aspellpath'] = '/usr/bin/aspell';
+$defaults['moodle']['pathtogs'] = '/usr/bin/gs';
+$defaults['moodle']['pathtodot'] = '/usr/bin/dot';
+$defaults['moodle']['pathtopdftoppm'] = '/usr/bin/pdftoppm';
+$defaults['moodle']['pathtopython'] = '/usr/bin/python3';
+
+$defaults['moodle']['auth_instructions'] = 'Usuário: admin
+Senha: myadmpass';
+$defaults['moodle']['forcelogin'] = 1;
+
