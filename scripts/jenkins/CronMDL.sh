97c5d895487111c6d9a72dfc23fe3769235ccc94
Adriano Ruseler <ruseler@inep.ufsc.br>
2023-02-16 13:04:38 -0300

    Update CronMDL.sh

--- scripts/jenkins/CronMDL.sh
+++ scripts/jenkins/CronMDL.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-FOLDERS=('mdl39' 'mdl310' 'mdl311' 'mdl40' 'oficina' 'integration')
+FOLDERS=('mdl311' 'mdl40' 'mdl41' 'oficina' 'integration' 'sophia')
 for MDLFOLDER in "${FOLDERS[@]}"; do
   sudo -u www-data /usr/bin/php /var/www/html/$MDLFOLDER/admin/cli/cron.php >/dev/null
 done
 
-#*/1 * * * * /usr/bin/php  /var/www/html/oficina/admin/cli/cron.php >/dev/null
+FOLDERS=('mdl39' 'mdl310' )
+for MDLFOLDER in "${FOLDERS[@]}"; do
+  sudo -u www-data /usr/bin/php7.4 /var/www/html/$MDLFOLDER/admin/cli/cron.php >/dev/null
+done
+
+
+FOLDERS=('mdl35' )
+for MDLFOLDER in "${FOLDERS[@]}"; do
+  sudo -u www-data /usr/bin/php7.2 /var/www/html/$MDLFOLDER/admin/cli/cron.php >/dev/null
+done
