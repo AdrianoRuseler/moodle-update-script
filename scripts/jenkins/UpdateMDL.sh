ef087f739ce0259067f5e2ca753815379772c152
Adriano Ruseler <ruseler@inep.ufsc.br>
2023-02-22 10:38:02 -0300

    Update UpdateMDL.sh
    list added plugins and missing ones

--- scripts/jenkins/UpdateMDL.sh
+++ scripts/jenkins/UpdateMDL.sh
@@ -10,6 +10,7 @@ fi
 # export MDLREPO="https://github.com/moodle/moodle.git"
 # export PLGBRANCH="main"
 # export PLGREPO="https://github.com/AdrianoRuseler/moodle311-plugins.git"
+# export PHPVER="php7.4"
 
 # Verify for LOCALSITENAME
 if [[ ! -v LOCALSITENAME ]] || [[ -z "$LOCALSITENAME" ]]; then
@@ -221,3 +222,9 @@ sudo -u www-data /usr/bin/$PHPVER $MDLHOME/admin/cli/maintenance.php --disable
 
 echo "start cron..."
 sudo service cron start
+
+echo "Prints tab-separated list of all third-party installed plugins..."
+sudo -u www-data /usr/bin/$PHPVER $MDLHOME/admin/cli/uninstall_plugins.php  --show-contrib
+
+echo "Prints tab-separated list of all missing from disk plugins..."
+sudo -u www-data /usr/bin/$PHPVER $MDLHOME/admin/cli/uninstall_plugins.php  --show-missing
