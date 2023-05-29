3f218a6ec6f35f89a10e367c68dfab916292eef0
Adriano Ruseler <ruseler@inep.ufsc.br>
2023-05-29 20:13:52 -0300

    Create InstallPHP.sh

---
+++ scripts/oracle/InstallPHP.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+echo "Add the following PHP PPA repository"
+sudo add-apt-repository ppa:ondrej/php -y && sudo apt-get update
+
+PHPVERS=('7.4' '8.0' '8.1' '8.2')
+for PHPVER in "${PHPVERS[@]}"; do
+	sudo apt-get install -y php$PHPVER libapache2-mod-php$PHPVER
+	sudo apt-get install -y php$PHPVER-{curl,zip,intl,xmlrpc,soap,xml,gd,ldap,common,cli,mbstring,mysql,imagick,json,readline,tidy,redis,memcached,apcu,opcache,mongodb} 
+done
+
+
+for PHPVER in "${PHPVERS[@]}"; do
+	# Set PHP ini
+	sed -i 's/memory_limit =.*/memory_limit = 512M/' /etc/php/$PHPVER/fpm/php.ini
+	sed -i 's/post_max_size =.*/post_max_size = 256M/' /etc/php/$PHPVER/fpm/php.ini
+	sed -i 's/upload_max_filesize =.*/upload_max_filesize = 256M/' /etc/php/$PHPVER/fpm/php.ini
+	sed -i 's/;max_input_vars =.*/max_input_vars = 6000/' /etc/php/$PHPVER/fpm/php.ini
+
+	sed -i 's/memory_limit =.*/memory_limit = 512M/' /etc/php/$PHPVER/apache2/php.ini
+	sed -i 's/post_max_size =.*/post_max_size = 256M/' /etc/php/$PHPVER/apache2/php.ini
+	sed -i 's/upload_max_filesize =.*/upload_max_filesize = 256M/' /etc/php/$PHPVER/apache2/php.ini
+	sed -i 's/;max_input_vars =.*/max_input_vars = 6000/' /etc/php/$PHPVER/apache2/php.ini
+
+	sed -i 's/memory_limit =.*/memory_limit = 512M/' /etc/php/$PHPVER/cli/php.ini
+	sed -i 's/post_max_size =.*/post_max_size = 256M/' /etc/php/$PHPVER/cli/php.ini
+	sed -i 's/upload_max_filesize =.*/upload_max_filesize = 256M/' /etc/php/$PHPVER/cli/php.ini
+	sed -i 's/;max_input_vars =.*/max_input_vars = 6000/' /etc/php/$PHPVER/cli/php.ini
+done
+
+
+systemctl reload apache2
+
