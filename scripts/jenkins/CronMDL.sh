64d22d33406fd6eda255ee01363c9ac97b3b33b2
Adriano Ruseler <adrianoruseler@gmail.com>
2024-10-24 12:33:24 -0300

    Update CronMDL.sh

--- scripts/jenkins/CronMDL.sh
+++ scripts/jenkins/CronMDL.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 
-#*/1 * * * * /usr/bin/php8.1  /var/www/html/mdl41/admin/cli/cron.php >/dev/null
-#*/1 * * * * /usr/bin/php8.1  /var/www/html/mdl42/admin/cli/cron.php >/dev/null
-#*/1 * * * * /usr/bin/php8.1  /var/www/html/integration/admin/cli/cron.php >/dev/null
-#*/1 * * * * /usr/bin/php8.1  /var/www/html/sophia/admin/cli/cron.php >/dev/null
-#*/1 * * * * /usr/bin/php8.1  /var/www/html/oficina/admin/cli/cron.php >/dev/null
+## https://moodledev.io/general/releases
 
-FOLDERS=('mdl41' 'mdl42' 'oficina' 'integration' 'sophia')
+# for php8.3
+FOLDERS=('mdl44' 'mdl45' 'integration' 'sophia')
 for MDLFOLDER in "${FOLDERS[@]}"; do
-  sudo -u www-data /usr/bin/php8.1 /var/www/html/$MDLFOLDER/admin/cli/cron.php >/dev/null
+  sudo -u www-data /usr/bin/php8.3 /var/www/html/"$MDLFOLDER"/admin/cli/cron.php >/dev/null
 done
 
-#*/1 * * * * /usr/bin/php8.0  /var/www/html/mdl40/admin/cli/cron.php >/dev/null
-#*/1 * * * * /usr/bin/php8.0  /var/www/html/mdl311/admin/cli/cron.php >/dev/null
-
-FOLDERS=('mdl311' 'mdl40')
+# for php8.2
+FOLDERS=('mdl42' 'mdl43' 'oficina')
 for MDLFOLDER in "${FOLDERS[@]}"; do
-  sudo -u www-data /usr/bin/php8.0 /var/www/html/$MDLFOLDER/admin/cli/cron.php >/dev/null
+  sudo -u www-data /usr/bin/php8.2 /var/www/html/"$MDLFOLDER"/admin/cli/cron.php >/dev/null
 done
 
-#*/1 * * * * /usr/bin/php7.4  /var/www/html/mdl39/admin/cli/cron.php >/dev/null
-
-FOLDERS=('mdl39')
+# for php8.1
+FOLDERS=('mdl41')
 for MDLFOLDER in "${FOLDERS[@]}"; do
-  sudo -u www-data /usr/bin/php7.4 /var/www/html/$MDLFOLDER/admin/cli/cron.php >/dev/null
+  sudo -u www-data /usr/bin/php8.1 /var/www/html/"$MDLFOLDER"/admin/cli/cron.php >/dev/null
 done
-
-#FOLDERS=('mdl35' )
-#for MDLFOLDER in "${FOLDERS[@]}"; do
-#  sudo -u www-data /usr/bin/php7.2 /var/www/html/$MDLFOLDER/admin/cli/cron.php >/dev/null
-#done
